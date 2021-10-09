d75263fdd648ae575a586e504fa4a44078753478
VFDGROUPLTD\Ayomide.Akinrotoye <ayomide.akinrotoye@vfdtech.ng>
2021-10-09 04:24:01 +0100

    wip

--- src/main/java/com/seamhealthgroup/diagbot/controllers/DiagnosisController.java
+++ src/main/java/com/seamhealthgroup/diagbot/controllers/DiagnosisController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import com.seamhealthgroup.diagbot.services.DiagnosisService;
 
 @RestController
 @Validated
+@CrossOrigin(origins = "http://localhost:3001")
 public class DiagnosisController {
 
 	private DiagnosisService diagnosisService;
